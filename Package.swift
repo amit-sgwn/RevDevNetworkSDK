d6c35aa0dc0a046705944e81032f6c22cf89f51f
Amit Kumar <amitsangwan898@gmail.com>
2024-05-13 14:42:57 +0530

    minor chage

--- Package.swift
+++ Package.swift
@@ -20,17 +20,18 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products from packages which this package depends on.
-        .target(
-            name: "RevDevNetworkSDK",
-            dependencies: []
-        ),
+//        .target(
+//            name: "RevDevNetworkSDK",
+//            dependencies: []
+//        ),
         .binaryTarget(
             name: "RevDevNetworkSDK", 
-            path: "RevDevNetworkSDK.xcframework")
-        .testTarget(
-            name: "RevDevNetworkSDKTests",
-            dependencies: ["RevDevNetworkSDK"]
-        )
+            url: "https://github.com/amit-sgwn/RevDevNetworkSDK/releases/download/1.0.1/RevDevNetworkSDK.xcframework.zip",
+            checksum: "94db05be4b3dd535331f3466d772030c1b1159d77261eda93c803627c5f145dd"),
+//        .testTarget(
+//            name: "RevDevNetworkSDKTests",
+//            dependencies: ["RevDevNetworkSDK"]
+//        )
     ]
 )
 
